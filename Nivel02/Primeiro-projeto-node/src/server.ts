11433245a92c1cdc4c6f1f2919397d927d0776e4
Luis Felipe de Paula Costa <lfbmrj15@gmail.com>
2021-08-20 12:45:06 -0300

    Mudanças do Eslint+prittier automaticas

--- Nivel02/Primeiro-projeto-node/src/server.ts
+++ Nivel02/Primeiro-projeto-node/src/server.ts
@@ -4,10 +4,10 @@ const app = express();
 
 app.use(express.json());
 
-app.get('',(request ,response)=>{
-  return  response.json({message:"Hello World"})
-})
+app.get('', (request, response) => {
+  return response.json({ message: 'Hello World' });
+});
 
-app.listen(3333, ()=>{
-  console.log("Server Started on Port 3333 😁")
+app.listen(3333, () => {
+  console.log('Server Started on Port 3333 😁');
 });
