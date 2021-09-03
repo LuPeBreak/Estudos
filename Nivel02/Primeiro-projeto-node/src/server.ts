656ab72b28d1abbca4b7ca6e9022847993f22b88
Luis Felipe de Paula Costa <lfbmrj15@gmail.com>
2021-09-03 11:06:41 -0300

    Salvando estudos para poder continuar em casa - AppError class - global error handling

--- Nivel02/Primeiro-projeto-node/src/server.ts
+++ Nivel02/Primeiro-projeto-node/src/server.ts
@@ -1,8 +1,11 @@
+// dependencies
 import 'reflect-metadata';
-
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
+import 'express-async-errors';
 import routes from './routes';
 
+// Errors handler
+import AppError from './errors/AppErrors';
 // configs
 import uploadsConfig from './config/uploads';
 
@@ -14,5 +17,20 @@ const app = express();
 app.use(express.json());
 app.use('/files', express.static(uploadsConfig.directory));
 app.use(routes);
+
+// global exeption handler
+app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
+  if (err instanceof AppError) {
+    return response.status(err.statusCode).json({
+      status: 'Error',
+      message: err.message,
+    });
+  }
+
+  return response.status(500).json({
+    status: 'Error',
+    message: 'internal server error',
+  });
+});
 
 app.listen(3333);
