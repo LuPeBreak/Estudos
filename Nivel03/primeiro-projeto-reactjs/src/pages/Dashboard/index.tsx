70f278fa5deb85d0a9de4140f86276abc8ba3ca9
Luis Felipe de Paula Costa <lfbmrj15@gmail.com>
2021-09-10 11:37:54 -0300

    page e style dashboard criado e finalizado - inicio da implementaçao da api

--- Nivel03/primeiro-projeto-reactjs/src/pages/Dashboard/index.tsx
+++ Nivel03/primeiro-projeto-reactjs/src/pages/Dashboard/index.tsx
@@ -1,7 +1,72 @@
-import React from 'react';
+// dependencies
+import React, { useState, FormEvent } from 'react';
 
-import { Title } from './styles';
+// icons
+import { FiChevronRight } from 'react-icons/fi';
 
-const Dashboard: React.FC = () => <Title>Explore repositorios no github</Title>;
+// api
+import api from '../../services/api';
+
+// logos
+import logo from '../../assets/logo.svg';
+
+// styles
+import { Title, Form, Repositories } from './styles';
+
+// interfaces
+interface Repository {
+  full_name: string;
+  description: string;
+  owner: {
+    loging: string;
+    avatar_url: string;
+  };
+}
+
+const Dashboard: React.FC = () => {
+  const [newRepo, setNewRepo] = useState('');
+  const [repositories, setRepositories] = useState<Repository[]>([]);
+
+  async function handleAddRepository(
+    event: FormEvent<HTMLFormElement>,
+  ): Promise<void> {
+    event.preventDefault();
+
+    const response = await api.get<Repository>(`repos/${newRepo}`);
+
+    const repository = response.data;
+
+    setRepositories([...repositories, repository]);
+  }
+
+  return (
+    <>
+      <img src={logo} alt="Github Explorer" />
+      <Title>Explore repositorios no github</Title>
+      <Form onSubmit={handleAddRepository}>
+        <input
+          value={newRepo}
+          onChange={e => setNewRepo(e.target.value)}
+          placeholder="Digite o nome do Repositorio"
+        />
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <a href="teste">
+          <img
+            src="https://avatars.githubusercontent.com/u/21126750?v=4"
+            alt="Lupebreak"
+          />
+          <div>
+            <strong>LuPeBreak/Estudos</strong>
+            <p>Repositorio para salvar estudos do curso Gostack</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  );
+};
 
 export default Dashboard;
