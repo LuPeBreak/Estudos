f684c15332de35fa9cf108d8a4d4d03a2e1a7140
Luis Felipe de Paula Costa <lfbmrj15@gmail.com>
2021-10-14 11:41:25 -0300

    Pagina de Login e cadastro criadas e finalizadas

---
+++ Nivel03/gobarber-reactjs/src/pages/SignUp/index.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { Form } from '@unform/web';
+
+import { FiMail, FiLock, FiUser, FiArrowLeft } from 'react-icons/fi';
+
+import logoImg from '../../assets/logo.svg';
+import Button from '../../components/Button';
+import Input from '../../components/Input';
+
+import { Container, Content, Background } from './styles';
+
+const SignUp: React.FC = () => {
+  function handleSubmit(data: any): void {
+    console.log(data);
+  }
+
+  return (
+    <Container>
+      <Background />
+      <Content>
+        <img src={logoImg} alt="GoBarber" />
+        <Form onSubmit={handleSubmit}>
+          <h1>Faça seu cadastro</h1>
+
+          <Input icon={FiUser} name="name" type="text" placeholder="Nome" />
+          <Input icon={FiMail} name="email" type="text" placeholder="E-mail" />
+          <Input
+            icon={FiLock}
+            name="password"
+            type="password"
+            placeholder="Senha"
+          />
+
+          <Button type="submit">Cadastrar</Button>
+        </Form>
+        <a href="login">
+          <FiArrowLeft />
+          Voltar para logon
+        </a>
+      </Content>
+    </Container>
+  );
+};
+
+export default SignUp;
